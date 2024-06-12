1e8fc5d522b65387c2a66d7ddd0a9de6ab8a66df
Jakob Brünker <10101851+JakobBruenker@users.noreply.github.com>
2024-06-12 16:15:40 +0200

    add favicon

--- app/Main.hs
+++ app/Main.hs
@@ -6,13 +6,13 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Handler.WarpTLS
 import Network.Wai.Middleware.Static
-import Network.HTTP.Types (status200, status301, status404)
+import Network.HTTP.Types (ok200, movedPermanently301, notFound404)
 import qualified Data.ByteString.Char8 as BS
-import System.FilePath ((<.>), takeExtension)
+import System.FilePath ((<.>), hasExtension)
 import System.Directory (doesFileExist)
 import Control.Concurrent (forkIO)
 import Data.Maybe (fromMaybe)
-import System.Environment (getEnv, getArgs)
+import System.Environment (getEnv)
 
 portHTTP :: Int
 portHTTP = 80
@@ -22,48 +22,39 @@ portHTTPS = 443
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        [] -> do
-            certPath <- getEnv "SSL_CERT_PATH"
-            keyPath <- getEnv "SSL_KEY_PATH"
-            putStrLn $ "Starting HTTP server on http://localhost:" <> show portHTTP
-            putStrLn $ "Starting HTTPS server on https://localhost:" <> show portHTTPS
-            -- HTTP to HTTPS redirect server
-            _ <- forkIO $ run portHTTP redirectApp
-            -- HTTPS server
-            runTLS (tlsSettings certPath keyPath) (setPort portHTTPS defaultSettings) app
-        ["--http"] -> do
-            putStrLn $ "Starting HTTP server on http://localhost:" <> show portHTTP
-            run portHTTP app
-        _ -> putStrLn "Invalid arguments. Usage: functionally-complete [--http]"
+    certPath <- getEnv "SSL_CERT_PATH"
+    keyPath <- getEnv "SSL_KEY_PATH"
+    putStrLn $ "Starting HTTP server on http://localhost:" <> show portHTTP
+    putStrLn $ "Starting HTTPS server on https://localhost:" <> show portHTTPS
+    -- HTTP to HTTPS redirect server
+    _ <- forkIO $ run portHTTP redirectApp
+    -- HTTPS server
+    runTLS (tlsSettings certPath keyPath) (setPort portHTTPS defaultSettings) app
 
 app :: Application
 app req respond = do
-    let path = BS.unpack $ rawPathInfo req
-    let fileToServe = if null path || path == "/" 
-                      then "index.html"
-                      else if takeExtension path == ".html"
-                           then dropWhile (== '/') path
-                           else dropWhile (== '/') path <.> "html"
+    let path = dropWhile (== '/') . BS.unpack $ rawPathInfo req
+    let fileToServe | null path = "index.html"
+                    | hasExtension path = path
+                    | otherwise = path <.> "html"
     staticApp fileToServe req respond
 
 staticApp :: FilePath -> Application
 staticApp path req respond = do
-    case tryPolicy (addBase "resources") path of
+    case tryPolicy (addBase "resources" >-> noDots >-> isNotAbsolute) path of
         Nothing -> notFound req respond
         Just filePath -> do
           fileExists <- doesFileExist filePath
           if fileExists
-            then respond $ responseFile status200 [("Content-Type", "text/html")] filePath Nothing
+            then respond $ responseFile ok200 [("Content-Type", getMimeType filePath)] filePath Nothing
             else notFound req respond
 
 -- Fallback response if the file is not found
 notFound :: Application
-notFound _ respond = respond $ responseFile status404 [("Content-Type", "text/html")] "resources/404.html" Nothing
+notFound _ respond = respond $ responseFile notFound404 [("Content-Type", "text/html")] "resources/404.html" Nothing
 
 -- Redirect HTTP to HTTPS
 redirectApp :: Application
 redirectApp req respond = do
     let secureHost = "https://" <> BS.unpack (fromMaybe "functionally-complete.com" $ requestHeaderHost req) <> BS.unpack (rawPathInfo req)
-    respond $ responseLBS status301 [("Location", BS.pack secureHost)] ""
+    respond $ responseLBS movedPermanently301 [("Location", BS.pack secureHost)] ""
