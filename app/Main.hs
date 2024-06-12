cbb2f3e9b9a131b2ba5fdbaf6ca814b3270b9dd7
Jakob Brünker <10101851+JakobBruenker@users.noreply.github.com>
2024-06-12 05:52:31 +0200

    Add HTTPS support

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,32 @@ module Main where
 
 import Network.Wai
 import Network.Wai.Handler.Warp
+import Network.Wai.Handler.WarpTLS
 import Network.Wai.Middleware.Static
-import Network.HTTP.Types (status200, status404)
+import Network.HTTP.Types (status200, status301, status404)
 import qualified Data.ByteString.Char8 as BS
 import System.FilePath ((<.>), takeExtension)
 import System.Directory (doesFileExist)
+import Control.Concurrent (forkIO)
+import Data.Maybe (fromMaybe)
+import System.Environment (getEnv)
 
-port :: Int
-port = 80
+portHTTP :: Int
+portHTTP = 80
+
+portHTTPS :: Int
+portHTTPS = 443
 
 main :: IO ()
 main = do
-    putStrLn $ "Starting server on http://localhost:" <> show port
-    run port app
+    certPath <- getEnv "SSL_CERT_PATH"
+    keyPath <- getEnv "SSL_KEY_PATH"
+    putStrLn $ "Starting HTTP server on http://localhost:" <> show portHTTP
+    putStrLn $ "Starting HTTPS server on https://localhost:" <> show portHTTPS
+    -- HTTP to HTTPS redirect server
+    _ <- forkIO $ run portHTTP redirectApp
+    -- HTTPS server
+    runTLS (tlsSettings certPath keyPath) (setPort portHTTPS defaultSettings) app
 
 app :: Application
 app req respond = do
@@ -41,3 +54,9 @@ staticApp path req respond = do
 -- Fallback response if the file is not found
 notFound :: Application
 notFound _ respond = respond $ responseFile status404 [("Content-Type", "text/html")] "resources/404.html" Nothing
+
+-- Redirect HTTP to HTTPS
+redirectApp :: Application
+redirectApp req respond = do
+    let secureHost = "https://" <> BS.unpack (fromMaybe "functionally-complete.com" $ requestHeaderHost req) <> BS.unpack (rawPathInfo req)
+    respond $ responseLBS status301 [("Location", BS.pack secureHost)] ""
