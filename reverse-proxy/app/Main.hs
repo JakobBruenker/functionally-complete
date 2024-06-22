180d2d253c1ff9a73ca08ca3eae16470c4a3b869
Jakob Brünker <10101851+JakobBruenker@users.noreply.github.com>
2024-06-22 23:56:28 +0200

    reverse-proxy: Improve types

--- reverse-proxy/app/Main.hs
+++ reverse-proxy/app/Main.hs
@@ -8,11 +8,13 @@
 
 module Main (main) where
 
+import Prelude hiding (log)
+
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.MVar
 import Control.Exception (SomeException, try, handle)
-import Control.Monad (void, forever)
-import Control.Monad.Reader (ReaderT, runReaderT, ask, liftIO)
+import Control.Monad (void, forever, (<=<))
+import Control.Monad.Reader (MonadIO, ReaderT, runReaderT, ask, asks, liftIO)
 import Data.ByteString.Char8 qualified as BS
 import Data.ByteString.Lazy.Char8 qualified as LBS
 import Data.Char (isSpace)
@@ -20,6 +22,7 @@ import Data.List (isPrefixOf)
 import Data.Maybe (fromMaybe, mapMaybe)
 import Data.Text (Text)
 import Data.Text qualified as T
+import Data.Void (Void, vacuous)
 import Network.HTTP.Client (Manager, newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.HTTP.ReverseProxy
@@ -28,7 +31,7 @@ import Network.HTTP.Types.Header (hReferer)
 import Network.HTTP.Types.Status (ok200, movedPermanently301, unauthorized401, serviceUnavailable503, badGateway502, temporaryRedirect307, statusCode)
 import Network.Wai
 import Network.Wai.Handler.Warp
-import Network.Wai.Handler.WarpTLS (runTLS, tlsSettingsChain, TLSSettings)
+import Network.Wai.Handler.WarpTLS (runTLS, tlsSettings)
 import System.Directory (createDirectoryIfMissing, getHomeDirectory, canonicalizePath)
 import System.Environment (getEnv)
 import System.FilePath ((</>), equalFilePath)
@@ -47,11 +50,19 @@ data AppConfig = AppConfig
   }
 
 type App = ReaderT AppConfig IO
+type Application' = Request -> (Response -> App ResponseReceived) -> App ResponseReceived
+type Middleware' = Application' -> Application'
 
 data Ports = Ports
   { active   :: Int
   , inactive :: Int
   }
+
+log :: MonadIO m => String -> m ()
+log = liftIO . putStrLn
+
+domainName :: BS.ByteString
+domainName = "functionally-complete.com"
 
 apiKeyFileName, portsFileName :: FilePath
 apiKeyFileName = "api_key"
@@ -61,8 +72,8 @@ main :: IO ()
 main = do
   opts <- parseOpts
   configDir <- if "~" `isPrefixOf` opts.configDir
-                then (</> drop 2 opts.configDir) <$> getHomeDirectory
-                else pure opts.configDir
+      then (</> drop 2 opts.configDir) <$> getHomeDirectory
+      else pure opts.configDir
   createDirectoryIfMissing True configDir
   
   let portsFile = configDir </> portsFileName
@@ -76,16 +87,28 @@ main = do
   
   manager <- newManager tlsManagerSettings
   
-  let appConfig = AppConfig{..}
-  
-  runProxy appConfig configDir opts.httpPort opts.httpsPort
+  runApp AppConfig{..} $ runProxy opts.httpPort opts.httpsPort
+
+runApp :: AppConfig -> App a -> IO a
+runApp = flip runReaderT
+
+runApplication' :: AppConfig -> Application' -> Application
+runApplication' config app req res = runReaderT (app req (liftIO . res)) config
+
+liftApplication :: Application -> Application'
+liftApplication app req res = do
+    appConfig <- ask
+    liftIO $ app req (runApp appConfig . res)
+
+getConfig :: (AppConfig -> MVar a) -> App a
+getConfig = liftIO . readMVar <=< asks
 
 readApiKey :: FilePath -> IO BS.ByteString
 readApiKey file = do
   apiKey <- BS.pack . takeWhile (not . isSpace) . dropWhile isSpace <$> readFile file
   if BS.null apiKey
     then fail "API key file is empty"
-    else return apiKey
+    else pure apiKey
 
 readPorts :: FilePath -> IO Ports
 readPorts file = do
@@ -96,7 +119,7 @@ readPorts file = do
         then writeDefaultPorts file
         else fail $ "Error reading ports file: " ++ show e
     Right content -> case parsePorts content of
-      Just ports -> return ports
+      Just ports -> pure ports
       Nothing -> fail "Invalid ports file format"
   where
     writeDefaultPorts f = do
@@ -109,7 +132,7 @@ parsePorts content = do
   let pairs = mapMaybe parseLine $ lines content
   active <- lookup "active" pairs
   inactive <- lookup "inactive" pairs
-  return $ Ports active inactive
+  pure $ Ports active inactive
 
 parseLine :: String -> Maybe (String, Int)
 parseLine line = case break (== ':') $ dropWhile (== ' ') line of
@@ -124,79 +147,80 @@ portsToString Ports{..} = unlines
   , "inactive: " ++ show inactive
   ]
 
-runProxy :: AppConfig -> FilePath -> Int -> Int -> IO ()
-runProxy appConfig configDir httpPort httpsPort = flip runReaderT appConfig do
+plainTextHeader :: HTTP.Header
+plainTextHeader = ("Content-Type", "text/plain")
+
+runProxy :: Int -> Int -> App ()
+runProxy httpPort httpsPort = do
     certPath <- liftIO $ getEnv "SSL_CERT_PATH"
     keyPath <- liftIO $ getEnv "SSL_KEY_PATH"
-    liftIO . putStrLn $ "Watching config files in " ++ configDir
-    liftIO . void $ forkIO $ watchConfigFiles appConfig
-
-    liftIO . putStrLn $ "Starting HTTP redirect server on port " ++ show httpPort
-    liftIO . void $ forkIO $ run httpPort redirectApp
+    log $ "Starting HTTP redirect server on port " ++ show httpPort
+    _ <- liftIO . forkIO $
+      run httpPort redirectApp
     
-    app <- reverseProxyApp
-    liftIO $ putStrLn $ "Starting HTTPS reverse proxy on port " ++ show httpsPort
-    liftIO $ runTLS (tlsSettings certPath keyPath) (setPort httpsPort defaultSettings) app
+    log $ "Starting HTTPS reverse proxy on port " ++ show httpsPort
+    appConfig <- ask
+    _ <- liftIO . forkIO $
+      runTLS (tlsSettings certPath keyPath) (setPort httpsPort defaultSettings) (runApplication' appConfig reverseProxyApp)
 
-watchConfigFiles :: AppConfig -> IO ()
-watchConfigFiles AppConfig{..} = withManager $ \mgr -> do
-    canonicalConfigDir <- canonicalizePath configDir
-    let apiKeyFile = canonicalConfigDir </> apiKeyFileName
-        portsFile = canonicalConfigDir </> portsFileName
-    void $ watchDir mgr configDir (const True) $ \event -> do
-        path <- canonicalizePath (eventPath event)
-        if | equalFilePath path apiKeyFile -> handle
-               do \(e :: SomeException) -> putStrLn $ "Error updating API key: " ++ show e
-               do newApiKey <- readApiKey apiKeyFile
-                  void $ swapMVar apiKeyVar newApiKey
-                  putStrLn "API key updated"
-           | equalFilePath path portsFile -> handle
-               do \(e :: SomeException) -> putStrLn $ "Error updating ports: " ++ show e
-               do newPorts <- readPorts portsFile
-                  void $ swapMVar portsVar newPorts
-                  putStrLn $ "Ports updated: " <> show newPorts.active <> " (active), " <> show newPorts.inactive <> " (inactive)"
-           | otherwise -> return ()
-    forever $ threadDelay 1000000
+    log . ("Watching config files in " <>) =<< asks (.configDir)
+    vacuous watchConfigFiles
 
-checkApiKey :: MVar BS.ByteString -> Middleware
-checkApiKey apiKeyVar app req respond = do
-    apiKey <- readMVar apiKeyVar
-    putStrLn $ "API key: " ++ show apiKey
-    putStrLn $ "Request headers: " ++ show (requestHeaders req)
+watchConfigFiles :: App Void
+watchConfigFiles = do
+    AppConfig{..} <- ask
+    liftIO $ withManager $ \mgr -> do
+        canonicalConfigDir <- canonicalizePath configDir
+        let apiKeyFile = canonicalConfigDir </> apiKeyFileName
+            portsFile = canonicalConfigDir </> portsFileName
+        void $ watchDir mgr configDir (const True) $ \event -> do
+            path <- canonicalizePath (eventPath event)
+            if | equalFilePath path apiKeyFile -> handle
+                   do \(e :: SomeException) -> log $ "Error updating API key: " ++ show e
+                   do newApiKey <- readApiKey apiKeyFile
+                      void $ swapMVar apiKeyVar newApiKey
+                      log "API key updated"
+               | equalFilePath path portsFile -> handle
+                   do \(e :: SomeException) -> log $ "Error updating ports: " ++ show e
+                   do newPorts <- readPorts portsFile
+                      void $ swapMVar portsVar newPorts
+                      log $ "Ports updated: " <> show newPorts.active <> " (active), " <> show newPorts.inactive <> " (inactive)"
+               | otherwise -> pure ()
+        forever $ threadDelay 1000000
+
+checkApiKey :: Middleware'
+checkApiKey app req respond = do
+    apiKey <- getConfig (.apiKeyVar)
     case lookup "X-API-Key" (requestHeaders req) of
         Just key | key == apiKey -> app req respond
-        _ -> respond $ responseLBS unauthorized401 [("Content-Type", "text/plain")] "Unauthorized"
+        _ -> respond $ responseLBS unauthorized401 [plainTextHeader] "Unauthorized"
 
-reverseProxyApp :: App Application
-reverseProxyApp = do
-    appConfig <- ask
-    pure \req respond -> runReaderT (proxyApp req respond) appConfig
-
-proxyApp :: Request -> (Response -> IO ResponseReceived) -> App ResponseReceived
-proxyApp req respond = do
-    AppConfig{..} <- ask
-    case pathInfo req of
-        ["health"] -> liftIO $ healthCheck portsVar req respond
-        ["preview-health"] -> liftIO $ checkApiKey apiKeyVar (healthCheck portsVar) req respond
-        ["active"] -> liftIO $ checkApiKey apiKeyVar (getActiveAndRespond portsVar respond) req respond
-        ["switch"] -> liftIO $ checkApiKey apiKeyVar (switchPorts configDir portsVar respond) req respond
-        "preview" : rest -> liftIO $ checkApiKey apiKeyVar (previewInactiveServer portsVar manager rest) req respond
+reverseProxyApp :: Application'
+reverseProxyApp req respond = do
+    app <- case pathInfo req of
+        ["health"] -> pure healthCheck
+        ["preview-health"] -> pure $ checkApiKey healthCheck
+        ["active"] -> pure $ checkApiKey getActiveAndRespond
+        ["switch"] -> pure $ checkApiKey switchPorts
+        "preview" : rest -> pure . checkApiKey $ previewInactiveServer rest
         _ -> do
             case checkRefererForPreview req of
-                Just previewPath -> redirectToPreview previewPath req respond
+                Just previewPath -> pure $ redirectToPreview previewPath
                 Nothing -> do
-                    Ports{..} <- liftIO $ readMVar portsVar
-                    let proxyDest _ = return $ WPRProxyDest $ ProxyDest "127.0.0.1" active
-                    liftIO $ waiProxyTo proxyDest handleErrors manager req respond
+                    Ports{..} <- getConfig (.portsVar)
+                    let proxyDest _ = pure . WPRProxyDest $ ProxyDest "127.0.0.1" active
+                    pure . liftApplication . waiProxyTo proxyDest handleErrors =<< asks (.manager)
+    app req respond
 
-healthCheck :: MVar Ports -> Application
-healthCheck portsVar _ res = do
-    Ports{..} <- readMVar portsVar
-    backendHealthy <- checkBackendHealth active
-    if backendHealthy
-        then res $ responseLBS ok200 [("Content-Type", "text/plain")] "OK"
-        else res $ responseLBS serviceUnavailable503 [("Content-Type", "text/plain")] "Service Unavailable"
+healthCheck :: Application'
+healthCheck _ res = do
+    Ports{active} <- getConfig (.portsVar)
+    backendHealthy <- liftIO $ checkBackendHealth active
+    let (status, msg) | backendHealthy = (ok200, "OK")
+                      | otherwise = (serviceUnavailable503, "Service Unavailable")
+    res $ responseLBS status [plainTextHeader] msg
 
+-- TODO really, this should only trigger if the prefix of the referer also matches
 checkRefererForPreview :: Request -> Maybe BS.ByteString
 checkRefererForPreview req = do
     referer <- lookup hReferer (requestHeaders req)
@@ -204,59 +228,57 @@ checkRefererForPreview req = do
         then Just "/preview"
         else Nothing
 
-redirectToPreview :: BS.ByteString -> Request -> (Response -> IO ResponseReceived) -> ReaderT AppConfig IO ResponseReceived
+redirectToPreview :: BS.ByteString -> Application'
 redirectToPreview previewPrefix req respond = do
     let path = rawPathInfo req
         newLocation = previewPrefix <> path
-    liftIO $ respond $ responseLBS temporaryRedirect307
+    respond $ responseLBS temporaryRedirect307
         [ ("Location", newLocation)
-        , ("Content-Type", "text/plain")
+        , plainTextHeader
         ] 
         "Redirecting to preview version"
 
-handleErrors :: SomeException -> Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+handleErrors :: SomeException -> Application
 handleErrors e _ res = liftIO $ do
-    putStrLn $ "Proxy error: " ++ show e
+    log $ "Proxy error: " ++ show e
     res $ responseLBS badGateway502 
-        [("Content-Type", "text/plain")] 
+        [plainTextHeader] 
         "Unable to process your request at this time. Please try again later."
 
-previewInactiveServer :: MVar Ports -> Manager -> [Text] -> Application
-previewInactiveServer portsVar manager pathSegments req respond = do
-      Ports{..} <- liftIO $ readMVar portsVar
+previewInactiveServer :: [Text] -> Application'
+previewInactiveServer pathSegments req respond = do
+      Ports{inactive} <- getConfig (.portsVar)
       let newPath = BS.intercalate "/" $ map (BS.pack . T.unpack) pathSegments
           proxyReq = req { rawPathInfo = newPath
                          , requestHeaderHost = Just "localhost"
                          }
-      let proxyDest _ = return $ WPRProxyDest $ ProxyDest "127.0.0.1" inactive
-      waiProxyTo proxyDest handleErrors manager proxyReq respond
+      let proxyDest _ = pure $ WPRProxyDest $ ProxyDest "127.0.0.1" inactive
+      manager <- asks (.manager)
+      liftApplication (waiProxyTo proxyDest handleErrors manager) proxyReq respond
 
-getActiveAndRespond :: MVar Ports -> (Response -> IO ResponseReceived) -> Application
-getActiveAndRespond portsVar respond _ _ = do
-    Ports{..} <- readMVar portsVar
-    respond $ responseLBS ok200 [("Content-Type", "text/plain")] (LBS.pack $ show active)
+getActiveAndRespond :: Application'
+getActiveAndRespond _ respond = do
+    Ports{active} <- liftIO . readMVar =<< asks (.portsVar)
+    respond $ responseLBS ok200 [plainTextHeader] (LBS.pack $ show active)
 
 checkBackendHealth :: Int -> IO Bool
 checkBackendHealth port = do
     let request = HTTP.parseRequest_ $ "http://localhost:" ++ show port ++ "/health"
-    response <- liftIO $ try $ HTTP.httpLbs request
+    response <- try $ HTTP.httpLbs request
     pure case response of
         Left (_ :: HTTP.HttpException) -> False
         Right res -> statusCode (HTTP.getResponseStatus res) == 200
 
 redirectApp :: Application
 redirectApp req respond = do
-    let secureHost = "https://" <> BS.unpack (fromMaybe "functionally-complete.com" $ requestHeaderHost req) <> BS.unpack (rawPathInfo req)
+    let secureHost = "https://" <> BS.unpack (fromMaybe domainName $ requestHeaderHost req) <> BS.unpack (rawPathInfo req)
     respond $ responseLBS movedPermanently301 [("Location", BS.pack secureHost)] ""
 
-tlsSettings :: FilePath -> FilePath -> TLSSettings
-tlsSettings cert key = tlsSettingsChain cert [cert] key
-
--- TODO Why are we getting respond as a separate argument and then ignoring it the second time
-switchPorts :: FilePath -> MVar Ports -> (Response -> IO ResponseReceived) -> Application
-switchPorts configDir portsVar respond _ _ = do
+switchPorts :: Application'
+switchPorts _ respond = do
+    AppConfig{..} <- ask
     let portsFile = configDir </> portsFileName
-    currentPorts <- readMVar portsVar
+    currentPorts <- liftIO $ readMVar portsVar
     let newPorts = Ports { active = currentPorts.inactive, inactive = currentPorts.active }
-    writeFile portsFile $ portsToString newPorts
+    liftIO . writeFile portsFile $ portsToString newPorts
     respond $ responseLBS ok200 [] "Port switch initiated"
